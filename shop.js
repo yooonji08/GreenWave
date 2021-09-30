1e93d2979bbed15e647b47a470e835bf2d2cbd0a
withtaylors <83859241+withtaylors@users.noreply.github.com>
2021-10-01 01:04:51 +0900

    재업로드

---
+++ shop.js
@@ -0,0 +1,133 @@
+/* 하트를 누르면 이미지와 숫자가 바뀌고, 다시 취소하면 원래대로 돌아가는 스크립트 */
+heart = 0;
+function toggleImg() {
+    if (heart == 0) {
+        heart = 1;
+        document.getElementById('like').src = "img/rlove.png";
+    }
+    else {
+        heart = 0;
+        document.getElementById('like').src = "img/wlove.png";
+    };
+};
+heart1 = 0;
+function toggleImg() {
+    if (heart1 == 0) {
+        heart1 = 1;
+        document.getElementById('like1').src = "img/rlove.png";
+    }
+    else {
+        heart1 = 0;
+        document.getElementById('like1').src = "img/wlove.png";
+    };
+};
+heart2 = 0;
+function toggleImg() {
+    if (heart2 == 0) {
+        heart2 = 1;
+        document.getElementById('like2').src = "img/rlove.png";
+    }
+    else {
+        heart2 = 0;
+        document.getElementById('like2').src = "img/wlove.png";
+    };
+};
+heart3 = 0;
+function toggleImg() {
+    if (heart3 == 0) {
+        heart3 = 1;
+        document.getElementById('like3').src = "img/rlove.png";
+    }
+    else {
+        heart3 = 0;
+        document.getElementById('like3').src = "img/wlove.png";
+    };
+};
+heart4 = 0;
+function toggleImg() {
+    if (heart4 == 0) {
+        heart = 1;
+        document.getElementById('like4').src = "img/rlove.png";
+    }
+    else {
+        heart4 = 0;
+        document.getElementById('like4').src = "img/wlove.png";
+    };
+};
+heart5 = 0;
+function toggleImg() {
+    if (heart == 0) {
+        heart5 = 1;
+        document.getElementById('like5').src = "img/rlove.png";
+    }
+    else {
+        heart5 = 0;
+        document.getElementById('like5').src = "img/wlove.png";
+    };
+};
+heart6 = 0;
+function toggleImg() {
+    if (heart6 == 0) {
+        heart6 = 1;
+        document.getElementById('like6').src = "img/rlove.png";
+    }
+    else {
+        heart6 = 0;
+        document.getElementById('like6').src = "img/wlove.png";
+    };
+};
+heart7 = 0;
+function toggleImg() {
+    if (heart7 == 0) {
+        heart = 1;
+        document.getElementById('like7').src = "img/rlove.png";
+    }
+    else {
+        heart7 = 0;
+        document.getElementById('like7').src = "img/wlove.png";
+    };
+};
+heart8 = 0;
+function toggleImg() {
+    if (heart8 == 0) {
+        heart8 = 1;
+        document.getElementById('like8').src = "img/rlove.png";
+    }
+    else {
+        heart8 = 0;
+        document.getElementById('like8').src = "img/wlove.png";
+    };
+};
+heart9 = 0;
+function toggleImg() {
+    if (heart9 == 0) {
+        heart9 = 1;
+        document.getElementById('like9').src = "img/rlove.png";
+    }
+    else {
+        heart9 = 0;
+        document.getElementById('like9').src = "img/wlove.png";
+    };
+};
+heart10 = 0;
+function toggleImg() {
+    if (heart10 == 0) {
+        heart10 = 1;
+        document.getElementById('like10').src = "img/rlove.png";
+    }
+    else {
+        heart10 = 0;
+        document.getElementById('like10').src = "img/wlove.png";
+    };
+};
+heart11 = 0;
+function toggleImg() {
+    if (heart11 == 0) {
+        heart11 = 1;
+        document.getElementById('like11').src = "img/rlove.png";
+    }
+    else {
+        heart11 = 0;
+        document.getElementById('like11').src = "img/wlove.png";
+    };
+};
