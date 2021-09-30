35596ebc49bc310c716bd09fd6bb99cc8fbcd157
withtaylors <83859241+withtaylors@users.noreply.github.com>
2021-10-01 01:15:02 +0900

    10.01 수정

--- shop.js
+++ shop.js
@@ -11,7 +11,7 @@ function toggleImg() {
     };
 };
 heart1 = 0;
-function toggleImg() {
+function toggleImg1() {
     if (heart1 == 0) {
         heart1 = 1;
         document.getElementById('like1').src = "img/rlove.png";
@@ -22,7 +22,7 @@ function toggleImg() {
     };
 };
 heart2 = 0;
-function toggleImg() {
+function toggleImg2() {
     if (heart2 == 0) {
         heart2 = 1;
         document.getElementById('like2').src = "img/rlove.png";
@@ -33,7 +33,7 @@ function toggleImg() {
     };
 };
 heart3 = 0;
-function toggleImg() {
+function toggleImg3() {
     if (heart3 == 0) {
         heart3 = 1;
         document.getElementById('like3').src = "img/rlove.png";
@@ -44,7 +44,7 @@ function toggleImg() {
     };
 };
 heart4 = 0;
-function toggleImg() {
+function toggleImg4() {
     if (heart4 == 0) {
         heart = 1;
         document.getElementById('like4').src = "img/rlove.png";
@@ -55,7 +55,7 @@ function toggleImg() {
     };
 };
 heart5 = 0;
-function toggleImg() {
+function toggleImg5() {
     if (heart == 0) {
         heart5 = 1;
         document.getElementById('like5').src = "img/rlove.png";
@@ -66,7 +66,7 @@ function toggleImg() {
     };
 };
 heart6 = 0;
-function toggleImg() {
+function toggleImg6() {
     if (heart6 == 0) {
         heart6 = 1;
         document.getElementById('like6').src = "img/rlove.png";
@@ -77,7 +77,7 @@ function toggleImg() {
     };
 };
 heart7 = 0;
-function toggleImg() {
+function toggleImg7() {
     if (heart7 == 0) {
         heart = 1;
         document.getElementById('like7').src = "img/rlove.png";
@@ -88,7 +88,7 @@ function toggleImg() {
     };
 };
 heart8 = 0;
-function toggleImg() {
+function toggleImg8() {
     if (heart8 == 0) {
         heart8 = 1;
         document.getElementById('like8').src = "img/rlove.png";
@@ -99,7 +99,7 @@ function toggleImg() {
     };
 };
 heart9 = 0;
-function toggleImg() {
+function toggleImg9() {
     if (heart9 == 0) {
         heart9 = 1;
         document.getElementById('like9').src = "img/rlove.png";
@@ -110,7 +110,7 @@ function toggleImg() {
     };
 };
 heart10 = 0;
-function toggleImg() {
+function toggleImg10() {
     if (heart10 == 0) {
         heart10 = 1;
         document.getElementById('like10').src = "img/rlove.png";
@@ -121,7 +121,7 @@ function toggleImg() {
     };
 };
 heart11 = 0;
-function toggleImg() {
+function toggleImg11() {
     if (heart11 == 0) {
         heart11 = 1;
         document.getElementById('like11').src = "img/rlove.png";
